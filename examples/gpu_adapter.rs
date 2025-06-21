6fde167a0d993ed443508a8245e0272853d21468
Estrol <estrol@estrol.dev>
2025-06-21 19:59:07 +0700

    Feat: Added BackendEnum

---
+++ examples/gpu_adapter.rs
@@ -0,0 +1,50 @@
+extern crate est_render;
+
+use est_render::prelude::*;
+
+fn main() {
+    let mut runner = est_render::create_runner().expect("Failed to create runner");
+    let mut window = runner
+        .create_window("Engine Example", Point2::new(800, 600))
+        .build()
+        .expect("Failed to create window");
+
+    let adapters = est_render::query_gpu_adapter(Some(&window));
+    if adapters.is_empty() {
+        eprintln!("No GPU adapters found. Exiting.");
+        return;
+    }
+
+    let selected_adapter = adapters
+        .iter()
+        .find(|adapter| adapter.backend_enum == AdapterBackend::Vulkan)
+        .cloned();
+
+    if selected_adapter.is_none() {
+        eprintln!("No suitable GPU adapter found. Exiting.");
+        return;
+    }
+
+    let adapter = selected_adapter.unwrap();
+    let mut gpu = create_gpu(Some(&mut window))
+        .set_adapter(&adapter)
+        .build()
+        .expect("Failed to create GPU");
+
+    while runner.pool_events(None) {
+        for event in runner.get_events() {
+            match event {
+                Event::Closed { .. } => {
+                    return;
+                }
+                _ => {}
+            }
+        }
+
+        if let Some(mut cmd) = gpu.begin_command() {
+            if let Some(mut rp) = cmd.begin_renderpass() {
+                rp.set_clear_color(Color::LIGHTBLUE);
+            }
+        }
+    }
+}
